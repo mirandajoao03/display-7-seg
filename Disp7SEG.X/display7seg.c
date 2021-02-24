c1d819581b9696d0c2a8e38380a9188810f643f0
20185561 <20185561@DSN1271158622.cfp127aluno.local>
2021-02-24 16:41:12 -0300

    alteração do local do vetordo arquivo main para display7seg

--- Disp7SEG.X/display7seg.c
+++ Disp7SEG.X/display7seg.c
@@ -8,6 +8,11 @@
 
 #include <xc.h>
 
+char vetor[16] = { 0x3F, 0x06, 0x5B, 0x4F,
+                   0x66, 0x6D, 0x7D, 0x07,
+                   0x7F, 0x6F, 0x77, 0x7C,
+                   0x39, 0x5E, 0x79, 0x71 };
+
 void display7seg_init ( void )
 {
     TRISD = 0x00;
@@ -16,5 +21,5 @@ void display7seg_init ( void )
 
 void display7seg ( int c )
 {
-    PORTD = c;
+    PORTD = vetor[c];
 }
--- Disp7SEG.X/main.c
+++ Disp7SEG.X/main.c
@@ -11,11 +11,6 @@
 #include "delay.h"
 #include "display7seg.h"
 
-char vetor[16] = { 0x3F, 0x06, 0x5B, 0x4F,
-                   0x66, 0x6D, 0x7D, 0x07,
-                   0x7F, 0x6F, 0x77, 0x7C,
-                   0x39, 0x5E, 0x79, 0x71 };
-
 void main(void) 
 {
     int cont = 0;
@@ -23,7 +18,7 @@ void main(void)
     
     while ( 1 )
     {
-                display7seg( vetor[cont] );
+                display7seg( cont );
                 delay ( 1000 );
                 cont++;
                 if ( cont >= 16 )
